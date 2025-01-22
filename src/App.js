0e172bbd904657713d87205a3c1dc0a25283ce50
rmutsv007 <passakorn.p@rmutsvmail.com>
2025-01-23 01:18:46 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MapComtents from './components/MapComtents';
+import 'leaflet/dist/leaflet.css';
+
+function App() {
+  return (
+    <>
+        <MapComtents />
+    </>
+  );
+}
+
+export default App;
